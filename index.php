f60bc254c83cedf06d67588dc1c2a7883847e563
Daryrahman <35571413+Daryrahman@users.noreply.github.com>
2022-04-19 16:28:01 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>Teknik Informatika</title>
+</head>
+
+<body data-bs-spy="scroll" data-bs-target="#nav-ex" data-bs-offset="190" tabindex="0">
+    <div class="container-fluid p-0">
+        <nav id="nav-ex" class="navbar navbar-expand-lg navbar-dark bg-gas-giant p-2 fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand d-flex" href="#">
+                    <img src="assets/images/logo_uin.png" alt="Uin Suska Riau" class="nav-logo" />
+                    <h5 class="mt-3 ms-3 fw-bold">TEKNIK INFORMATIKA</h5>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="#spyHome">Beranda</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="#spyProfile">Profil</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="#spyNews">Berita</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="#spyContact">Kontak</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Content -->
+        <section id="spyHome">
+            <div class="container-fluid px-0">
+                <div class="jumbotron">
+                    <div class="col-md-5 ms-auto ps-4">
+                        <h1 class="fw-bolder">TEKNIK INFORMATIKA</h1>
+                        <h4 class="fw-normal">UIN SULTAN SYARIF KASIM RIAU</h4>
+                    </div>
+                    <svg class="wave-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                        <path fill="#fff" fill-opacity="1" d="M0,64L21.8,74.7C43.6,85,87,107,131,144C174.5,181,218,235,262,218.7C305.5,203,349,117,393,85.3C436.4,53,480,75,524,106.7C567.3,139,611,181,655,192C698.2,203,742,181,785,149.3C829.1,117,873,75,916,80C960,85,1004,139,1047,165.3C1090.9,192,1135,192,1178,176C1221.8,160,1265,128,1309,133.3C1352.7,139,1396,181,1418,202.7L1440,224L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"></path>
+                    </svg>
+                </div>
+            </div>
+        </section>
+
+        <section id="spyAkademik">
+            <div class="container-fluid px-0 pt-5">
+                <div class="greets-sect">
+                    <div class="row justify-content-center me-0 my-4">
+                        <div class="col-md-4 me-5">
+                            <img src="assets/images/man.png" alt="man" />
+                        </div>
+                        <div class="col-md-5">
+                            <h5 class="fw-bolder pt-5">KATA SAMBUTAN</h5>
+                            <p class="pt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam expedita perferendis natus nisi provident quisquam beatae modi nemo laboriosam, commodi esse numquam, repellat quae unde vitae praesentium quis! Dolore, delectus.
+                                Voluptas unde modi sequi fugit ullam molestiae explicabo animi dolores nulla a dolorem vero quo excepturi fuga beatae ad consequuntur, velit eveniet error quod, ab expedita reiciendis. Odit, veritatis deserunt?</p>
+                            <p class="blockquote-footer mt-3">John Doe - Head of Study Program Teknik Informatika UIN SUSKA RIAU</p>
+                        </div>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                        <path fill="#181D31" fill-opacity="1" d="M0,160L24,144C48,128,96,96,144,117.3C192,139,240,213,288,250.7C336,288,384,288,432,261.3C480,235,528,181,576,160C624,139,672,149,720,176C768,203,816,245,864,261.3C912,277,960,267,1008,224C1056,181,1104,107,1152,106.7C1200,107,1248,181,1296,181.3C1344,181,1392,107,1416,69.3L1440,32L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path>
+                    </svg>
+                </div>
+            </div>
+        </section>
+
+        <section id="spyProfile" class="h-100">
+            <div class="container-fluid px-0">
+                <div class="about-sect bg-gas-giant pb-5 py-4">
+                    <div class="row me-0">
+                        <a href="#spyProfile">
+                            <h2 class="text-center fw-bolder mb-5">
+                                VISI & MISI
+                            </h2>
+                        </a>
+                    </div>
+                    <div class="row me-0 px-5 justify-content-center my-5">
+                        <div class="col-md-5">
+                            <h4 class="fw-bolder"># VISI</h4>
+                            <p class="pt-2 ps-4">Terwujudnya program studi Teknik Informatika sebagai program studi yang unggul dalam penyelenggaraan pendidikan, penelitian, dan pengabdian kepada masyarakat yang mengintegrasikan informatika dan Islam pada tahun 2023.</p>
+                        </div>
+                        <div class="col-md-5">
+                            <h4 class="fw-bolder"># MISI</h4>
+                            <p class="pt-2 ps-4">Misi mengacu kepada keterlibatan Program Studi dalam mencari solusi terhadap persoalan-persoalan yang terjadi ditengah masyarakat. Adapun Misi Program Studi Teknik Informatika adalah:</p>
+                            <ol class="ps-4" type="1">
+                                <li class="py-1">
+                                    Menyelenggarakan pendidikan dan pengajaran berkualitas yang mengintegrasikan antara Informatika dan nilai-nilai keIslaman.
+                                </li>
+                                <li class="py-1">
+                                    Menyelenggarakan penelitian yang unggul di bidang Informatika, yang mengintegrasikan nilai-nilai keislaman.
+                                </li>
+                                <li class="py-1">
+                                    Memanfaatkan kajian informatika yang terintegrasi keislaman untuk melaksanakan pengabdian kepada masyarakat sebagai kontribusi terhadap pembangunan bangsa.
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                    <svg class="wave-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                        <path fill="#fff" fill-opacity="1" d="M0,128L24,106.7C48,85,96,43,144,69.3C192,96,240,192,288,213.3C336,235,384,181,432,138.7C480,96,528,64,576,48C624,32,672,32,720,69.3C768,107,816,181,864,202.7C912,224,960,192,1008,192C1056,192,1104,224,1152,202.7C1200,181,1248,107,1296,80C1344,53,1392,75,1416,85.3L1440,96L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path>
+                    </svg>
+                </div>
+            </div>
+        </section>
+
+        <section id="spyNews">
+            <div class="container-fluid px-0">
+                <div class="news-sect">
+                    <div class="row me-0">
+                        <a href="#spyNews">
+                            <h2 class="text-center fw-bolder mb-5">
+                                BERITA
+                            </h2>
+                        </a>
+                    </div>
+                    <div class="row justify-content-center me-0 my-5 gx-5">
+                        <div class="col-md-4 me-5">
+                            <img class="rounded" src="assets/images/graduation.jpg" alt="tif-graduation" />
+                        </div>
+                        <div class="col-md-5 ms-3">
+                            <h5 class="fw-bolder pt-4">WISUDA TEKNIK INFOTMATIKA 2022</h5>
+                            <p class="pt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam expedita perferendis natus nisi provident quisquam beatae modi nemo laboriosam, commodi esse numquam, repellat quae unde vitae praesentium quis! Dolore, delectus.
+                                Voluptas unde modi sequi fugit ullam molestiae explicabo animi dolores nulla a dolorem vero quo excepturi fuga beatae ad consequuntur, velit eveniet error quod, ab expedita reiciendis. Odit, veritatis deserunt?</p>
+                            <button class="btn btn-gas-giant mt-2 p-2">SELENGKAPNYA</button>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center me-0 my-5 gx-5">
+                        <div class="col-md-4 me-5">
+                            <img class="rounded" src="assets/images/lecture.jpg" alt="tif-lecturer" />
+                        </div>
+                        <div class="col-md-5 ms-3">
+                            <h5 class="fw-bolder pt-4">PERKULIAHAN UMUM</h5>
+                            <p class="pt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam expedita perferendis natus nisi provident quisquam beatae modi nemo laboriosam, commodi esse numquam, repellat quae unde vitae praesentium quis! Dolore, delectus.
+                                Voluptas unde modi sequi fugit ullam molestiae explicabo animi dolores nulla a dolorem vero quo excepturi fuga beatae ad consequuntur, velit eveniet error quod, ab expedita reiciendis. Odit, veritatis deserunt?</p>
+                            <button class="btn btn-gas-giant mt-2 p-2">SELENGKAPNYA</button>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center me-0 my-5 gx-5">
+                        <div class="col-md-4 me-5">
+                            <img class="rounded" src="assets/images/seminar.jpg" alt="man" />
+                        </div>
+                        <div class="col-md-5 ms-3">
+                            <h5 class="fw-bolder pt-4">SEMINAR PENGEMBANGAN WEB</h5>
+                            <p class="pt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam expedita perferendis natus nisi provident quisquam beatae modi nemo laboriosam, commodi esse numquam, repellat quae unde vitae praesentium quis! Dolore, delectus.
+                                Voluptas unde modi sequi fugit ullam molestiae explicabo animi dolores nulla a dolorem vero quo excepturi fuga beatae ad consequuntur, velit eveniet error quod, ab expedita reiciendis. Odit, veritatis deserunt?</p>
+                            <button class="btn btn-gas-giant mt-2 p-2">SELENGKAPNYA</button>
+                        </div>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                        <path fill="#181D31" fill-opacity="1" d="M0,160L24,144C48,128,96,96,144,117.3C192,139,240,213,288,250.7C336,288,384,288,432,261.3C480,235,528,181,576,160C624,139,672,149,720,176C768,203,816,245,864,261.3C912,277,960,267,1008,224C1056,181,1104,107,1152,106.7C1200,107,1248,181,1296,181.3C1344,181,1392,107,1416,69.3L1440,32L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path>
+                    </svg>
+                </div>
+            </div>
+        </section>
+    </div>
+    <!-- End Content -->
+
+    <footer id="spyContact" class="container-fluid pt-5 pb-2">
+        <h2 class="text-center fw-bolder">KONTAK</h2>
+        <div class="row justify-content-center my-5">
+            <div class="col-md-6">
+                <p class="text-center">Jurusan Teknik Informatika, Gedung Fakultas Sains & Teknologi UIN Suska Riau. Jl.H.R.Soebrantas No.155 KM 18, Simpang Baru Panam, Pekanbaru 28293</p>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-4 text-center">
+                <div class="facebook">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                    </svg> <a class="fw-bolder ps-1" href="https://www.facebook.com/fst.uin.suska">FST UIN Suska Riau</a>
+                </div>
+                <p class="mt-5">&#9400;Copyright 2022, Original By Dary Rahman</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
